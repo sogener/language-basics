09ab59fc0a1d42dc39f7713a1275491b4fc5e37e
Gleb Kalinin <g.kalinin@iqdev.digital>
2022-02-12 13:01:05 +0500

    4 | Change array elems which is referenced by slice

--- main.go
+++ main.go
@@ -4,23 +4,27 @@ import "fmt"
 
 func main() {
 	bankOptions := [4]string{
-		"купить хлеб",
-		"купить молоко",
-		"купить пиво",
-		"дописать третий модуль",
+		"Пополнить",
+		"Просмотреть",
+		"Снять",
+		"Посмотреть %",
 	}
 
 	// Мы создали пустой срез. Когда он не ссылается ни на какой массив, его значение по умолчанию равно nil.
 	var actions []string
-	fmt.Println(actions == nil)
-
 	/*
 		Далее мы присвоили ему срез нашего массива с 1 по 4 элемент.
 		Синтаксис [m:n] позволяет делать выборку элементов массива или среза от индекса m до n.
 		Пустые скобки [:] означают выборку всех элементов.
 	*/
-	actions = bankOptions[1:4]
-	fmt.Println(actions == nil)
+	actions = bankOptions[0:4]
+
+	for i := range actions {
+		fmt.Println(actions[i])
+	}
+
+	bankOptions[2] = "Купить пиво"
+	fmt.Println("---- После изменения bankOptions ----")
 
 	for i := range actions {
 		fmt.Println(actions[i])
