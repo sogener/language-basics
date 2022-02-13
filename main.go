88761ac3e31719fd943571cde48c0ab4aa0db937
Gleb Kalinin <g.kalinin@iqdev.digital>
2022-02-13 13:20:41 +0500

    5 | Struct v2

--- main.go
+++ main.go
@@ -3,17 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	employee := struct {
+	type Employee struct {
 		name   string
 		sex    string
 		age    int
 		salary int
-	}{
-		name:   "Вася",
+	}
+
+	firstEmployee := Employee{
+		name:   "Vasya",
 		sex:    "Male",
 		age:    18,
 		salary: 1000,
 	}
+	secondEmployee := Employee{
+		name:   "Alex",
+		sex:    "Male",
+		age:    22,
+		salary: 555,
+	}
 
-	fmt.Printf("%+v\n", employee)
+	fmt.Printf("%+v\n", firstEmployee)
+	fmt.Printf("%+v\n", secondEmployee)
 }
