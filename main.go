c87f7757d30edc88adf2e99745c87da01e4dde20
Gleb Kalinin <g.kalinin@iqdev.digital>
2022-02-13 13:35:20 +0500

    5 | Method of struct
    Объявление методов структур имеет немного различный синтаксис с функциями, и выглядит он следующим образом:
    func (s structName) methodName(arg1 type, arg2 type ...) returnType {}
    
    Где:
    structName — имя структуры для которой объявляется метод.
    s — так называемый ресивер (или получатель, но я больше люблю использовать английскую терминологию). Вы можете называть его как угодно, но хорошей практикой будет использовать 1–2 символа, которые являются первыми символами названия структуры.

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	firstEmployee := newEmployee("Вася", "М", 25, 1500)
 
 	fmt.Printf("%+v\n", firstEmployee.name)
+	fmt.Println(firstEmployee.getInfo())
 }
 
 func newEmployee(name, sex string, age, salary int) employee {
@@ -22,4 +23,7 @@ func newEmployee(name, sex string, age, salary int) employee {
 		age:    age,
 		salary: salary,
 	}
+}
+func (e employee) getInfo() string {
+	return fmt.Sprintf("Сотрудник: %s\nПол: %s\nВозраст: %d\nЗарплата: %d", e.name, e.sex, e.age, e.salary)
 }
