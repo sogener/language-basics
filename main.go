728e684eae5d39105b60d71993e48e8b7c856301
Gleb Kalinin <g.kalinin@iqdev.digital>
2022-02-13 13:16:59 +0500

    5 | Struct

--- main.go
+++ main.go
@@ -3,24 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	users := map[string]int{
-		"user1": 1,
-		"user2": 2,
+	employee := struct {
+		name   string
+		sex    string
+		age    int
+		salary int
+	}{
+		name:   "Вася",
+		sex:    "Male",
+		age:    18,
+		salary: 1000,
 	}
 
-	users["Billy"] = 10
-	users["Alex"] = 20
-
-	/*
-		Как мы уже говорили, мапы, как и срезы, являются указателями на область в памяти.
-		Поэтому если скопировать мапу в новую переменную и удалить из нее элементы,
-		это отобразится также и на новой мапе.
-	*/
-
-	var usersNew map[string]int
-	usersNew = users
-
-	delete(usersNew, "user2")
-	fmt.Println(users)
-
+	fmt.Printf("%+v\n", employee)
 }
