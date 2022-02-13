0f61665d1d8b1e5b7f61b876a6bd21f639710e53
Gleb Kalinin <g.kalinin@iqdev.digital>
2022-02-13 18:39:16 +0500

    6 | Empty interface
    Работая с пустыми интерфейсами, мы имеем возможность проверять интерфейс на его динамический тип
    
    Наша функция принимает в качестве аргумента пустой интерфейс. В теле функции мы делаем серию проверок, проверяя это значение на конкретный тип и выполняя в зависимости от этого разные действия.
    
    Для проверки типа используется следующая конструкция:
    
    value, ok := variable.(type)
    
    При проверке типа мы получаем 2 переменные:  bool, который равен true если значение удовлетворяет данному типу и само значение, приведенное к нужному типу.

--- main.go
+++ main.go
@@ -1,83 +1,19 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
-
-type programmer struct {
-	id        int
-	name      string
-	isWorking bool
-}
-type dumbStorage struct{}
-
-type storage interface {
-	insert(p programmer) error
-	get(id int) (programmer, error)
-	delete(id int) error
-}
-
-type memoryStorage struct {
-	data map[int]programmer
-}
-
-func newMemoryStorage() *memoryStorage {
-	return &memoryStorage{
-		data: make(map[int]programmer),
-	}
-}
-
-func (s *memoryStorage) insert(e programmer) error {
-	s.data[e.id] = e
-	return nil
-}
-
-func (s *memoryStorage) get(id int) (programmer, error) {
-	e, exists := s.data[id]
-	if !exists {
-		return programmer{}, errors.New("programmer with such id doesn't exist")
-	}
-
-	return e, nil
-}
-
-func (s *memoryStorage) delete(id int) error {
-	delete(s.data, id)
-	return nil
-}
-
-func newDumbStorage() *dumbStorage {
-	return &dumbStorage{}
-}
-
-func (s dumbStorage) insert(p programmer) error {
-	fmt.Println("Вставка прошла успешно")
-	return nil
-}
-
-func (s dumbStorage) get(id int) (programmer, error) {
-	p := programmer{id: id}
-	return p, nil
-}
-
-func (s dumbStorage) delete(id int) error {
-	fmt.Println("Удаление произошло")
-	return nil
-}
-
-func spawnEmployees(s storage) {
-	for i := 1; i <= 10; i++ {
-		s.insert(programmer{id: i})
-	}
-}
+import "fmt"
 
 func main() {
-	ms := newMemoryStorage()
-	ds := newDumbStorage()
+	printType(3)
+	printType("интерфейсы - это просто")
+	printType([]string{"слайсы", "тоже"})
+}
 
-	spawnEmployees(ms)
-	fmt.Println(ms.get(3))
-
-	spawnEmployees(ds)
+func printType(value interface{}) {
+	if _, ok := value.(int); ok {
+		fmt.Println("тип аргумента int")
+	} else if _, ok := value.(string); ok {
+		fmt.Println("тип аргумента string")
+	} else {
+		fmt.Println("тип аргумента не int && ne string")
+	}
 }
