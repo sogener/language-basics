a2b7ae035aa760db06880361ddedc74f149bb305
Gleb Kalinin <g.kalinin@iqdev.digital>
2022-02-13 13:47:27 +0500

    5 | Custom type

--- main.go
+++ main.go
@@ -2,31 +2,13 @@ package main
 
 import "fmt"
 
-type employee struct {
-	name   string
-	sex    string // пол
-	age    int
-	salary int // зарплата
+type age int
+
+func (a age) isAdult() bool {
+	return a > 18
 }
 
 func main() {
-	firstEmployee := newEmployee("Вася", "М", 25, 1500)
-
-	firstEmployee.setName("имя для struct через receiver")
-	fmt.Println(firstEmployee.getInfo())
-}
-
-func newEmployee(name, sex string, age, salary int) employee {
-	return employee{
-		name:   name,
-		sex:    sex,
-		age:    age,
-		salary: salary,
-	}
-}
-func (e employee) getInfo() string {
-	return fmt.Sprintf("Сотрудник: %s\nПол: %s\nВозраст: %d\nЗарплата: %d", e.name, e.sex, e.age, e.salary)
-}
-func (e *employee) setName(name string) {
-	e.name = name
+	myAge := age(20)
+	fmt.Print("Я совершеннолетний?", myAge.isAdult())
 }
